7de947f5dfd934b414a08b943645bfcabd0f6eeb
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-02 12:40:40 +0100

    Helyek listázva

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <List></List>
 </template>
 
 <script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,17 +1,22 @@
 <template>
   <table>
       <thead>
-          <th>id</th>
-          <th>name</th>
-          <th>lat</th>
-          <th>lng</th>
-          <th>lng</th>
-          <th>user_id</th>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>lat</th>
+            <th>lng</th>
+            <th>user_id</th>
+          </tr>
       </thead>
       <body>
-          <tr>
-              <td></td>
-          </tr>
+            <tr v-for="location in locations" :key="location.id">
+                <td>{{ location.id }}</td>
+                <td>{{ location.name }}</td>
+                <td>{{ location.lat }}</td>
+                <td>{{ location.lng }}</td>
+                <td>{{ location.user_id }}</td>
+            </tr>
       </body>
   </table>
 </template>
@@ -23,6 +28,15 @@ export default {
         return {
             locations: [],
         }
+    },
+    methods: {
+        async loadData() {
+            let response = await fetch("http://127.0.0.1:8000/api/locations")
+            this.locations = await response.json()
+        }
+    },
+    mounted() {
+        this.loadData()
     }
 }
 </script>
