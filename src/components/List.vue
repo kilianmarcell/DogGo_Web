1cf9cf408604df80d729d968859d76f1af1b7a69
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-02 13:19:01 +0100

    Helyek CRUD kész

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,48 +1,51 @@
 <template>
-<div>
-    <h1>Helyek</h1>
-  <table>
-      <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Lat</th>
-            <th>Lng</th>
-            <th>User id</th>
-          </tr>
-      </thead>
-      <tbody>
-            <tr v-for="l in locations" :key="l.id">
-                <td>{{ l.id }}</td>
-                <td>{{ l.name }}</td>
-                <td>{{ l.lat }}</td>
-                <td>{{ l.lng }}</td>
-                <td>{{ l.user_id }}</td>
-                <td>
-                    <button @click="deleteLocation(l.id)">Törlés</button>
-                </td>
-            </tr>
+    <div>
+        <h1>Helyek</h1>
+    <table>
+        <thead>
             <tr>
-                <td></td>
-                <td><input type="text" v-model="location.name"></td>
-                <td><input type="number" v-model="location.lat"></td>
-                <td><input type="number" v-model="location.lng"></td>
-                <td></td>
-                <td>
-                    <button @click="newLocation" :disabled="saving" v-if="!add_new">Hozzáadás</button>
-                    <button v-if="add_new" @click="saveLocation">Mentés</button>
-                    <br>
-                    <button v-if="add_new" @click="cancelLocation">Mégse</button>
-                </td>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Lat</th>
+                <th>Lng</th>
+                <th>User id</th>
             </tr>
-      </tbody>
-  </table>
-</div>
+        </thead>
+        <tbody>
+                <tr v-for="l in locations" :key="l.id">
+                    <td>{{ l.id }}</td>
+                    <td>{{ l.name }}</td>
+                    <td>{{ l.lat }}</td>
+                    <td>{{ l.lng }}</td>
+                    <td>{{ l.user_id }}</td>
+                    <td>
+                        <button @click="deleteLocation(l.id)">Törlés</button>
+                        <br>
+                        <button @click="editLocation(l.id)">Szerkesztés</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="text" v-model="location.name"></td>
+                    <td><input type="number" v-model="location.lat"></td>
+                    <td><input type="number" v-model="location.lng"></td>
+                    <td></td>
+                    <td>
+                        <button @click="newLocation" :disabled="saving" v-if="!add_new">Hozzáadás</button>
+                        <button v-if="add_new" @click="saveLocation">Mentés</button>
+                        <br>
+                        <button v-if="add_new" @click="cancelLocation">Mégse</button>
+                    </td>
+                </tr>
+        </tbody>
+    </table>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'List',
+
     data() {
         return {
             locations: [],
@@ -91,6 +94,46 @@ export default {
             await this.loadData()
         },
 
+        async editLocation(id) {
+            this.add_new = true
+            let response = await fetch(`http://127.0.0.1:8000/api/locations/${id}`)
+            let data = await response.json()
+            this.location = {...data}
+
+            /*
+            
+            this.location = {...data} -> ha a egyik tag változik, a másik is változik "szinkronizálva vannak",
+            ugyan az az objektum
+
+            ha simán this.location = data -> akkor a location a data referenciája
+
+            ... -> akkor új objektumot csinál a data-ból
+            
+            */
+        },
+
+        async saveLocation() {
+            this.saving = 'disabled'
+
+            await fetch(`http://127.0.0.1:8000/api/locations/${this.location.id}`, {
+                method: 'PATCH',
+                headers: {
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json'
+                },
+                body: JSON.stringify(this.location)
+            })
+
+            await this.loadData()
+            
+            this.saving = false
+            this.resetForm()
+        },
+
+        cancelLocation() {
+            this.resetForm()
+        },
+
         resetForm() {
             this.location = {
                 name: "",
@@ -109,27 +152,27 @@ export default {
 </script>
 
 <style>
-  h1 {
-    text-align: center;
-  }
+    h1 {
+        text-align: center;
+    }
 
-  table {
-    margin: auto;
-    border: 1px solid black;
-  }
+    table {
+        margin: auto;
+        border: 1px solid black;
+    }
 
-  th {
-    padding: 40px 20px 30px 20px;
-    text-align: center;
-    font-size: 20pt;
-  }
+    th {
+        padding: 40px 20px 30px 20px;
+        text-align: center;
+        font-size: 20pt;
+    }
 
-  td {
-      text-align: center;
-    padding: 20px 100px 20px 100px;
-  }
+    td {
+        text-align: center;
+        padding: 20px 100px 20px 100px;
+    }
 
-  button {
-    width: 100%;
-  }
+    button {
+        width: 100%;
+    }
 </style>
