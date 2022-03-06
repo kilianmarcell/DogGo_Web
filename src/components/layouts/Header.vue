e25d3d1863f50ea1099b458e20c359bcdee2f29b
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-06 22:07:26 +0100

    NavBar elemek jobbra rendezve

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -2,12 +2,12 @@
      <div>
           <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                <div class="container-fluid">
-                    <a class="navbar-brand fs-1" href="">DogGo</a>
+                    <a class="navbar-brand fs-1 ms-3" href="">DogGo</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                          <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                         <ul class="navbar-nav me-3 ms-auto mb-2 mb-lg-0">
                               <li class="nav-item fs-4">
                                    <a class="nav-link active" aria-current="page" href="#"><fa :icon="['fas', 'home']"/></a>
                               </li>
@@ -79,7 +79,7 @@
 
 <script>
 export default {
-
+     name: 'Header'
 }
 </script>
 
