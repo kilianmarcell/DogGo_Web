8f9a910e080ef1a983bdb0f21fbeca089b63a414
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-13 00:08:36 +0100

    Kinézet javítva

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,14 +1,16 @@
 <template>
      <div>
           <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-               <div class="container-fluid">
-                    <img src="../../assets/DogGoLogo.png" id="logo" class="img-fluid rounded ms-5" alt="">
+               <div class="container-fluid mx-5">
+                    <img src="../../assets/DogGoLogo.png" id="logo" class="img-fluid rounded" alt="">
                     <a class="navbar-brand fs-1" href="">DogGo</a>
+
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                          <span class="navbar-toggler-icon"></span>
                     </button>
+                    
                     <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
-                         <ul class="navbar-nav me-5 ms-auto mb-2 mb-lg-0 fs-4">
+                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-4">
                               <li class="nav-item">
                                    <a class="nav-link active" aria-current="page" href="#"><fa :icon="['fas', 'home']"/></a>
                               </li>
