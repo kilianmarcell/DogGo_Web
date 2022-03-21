03eb722c7b663b7037f0203686546e2d3a8c180e
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-21 14:36:02 +0100

    Routing rendezve

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '../components/sites/HomePage.vue'
-import Register from '../components/authentication/Register.vue'
-import Login from '../components/authentication/Login.vue'
-import Info from '../components/parts/Info.vue'
+import RegisterPage from '../components/sites/RegisterPage.vue'
+import LoginPage from '../components/sites/LoginPage.vue'
+import InfoPage from '../components/sites/InfoPage.vue'
+import UserDataPage from '../components/sites/UserDataPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
-      name: 'HomePage',
+      name: 'Home',
       component: HomePage
     },
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: RegisterPage
     },
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: LoginPage
     },
     {
       path: '/info',
       name: 'Info',
-      component: Info
+      component: InfoPage
+    },
+    {
+      path: '/userdata',
+      name: 'UserData',
+      component: UserDataPage
     }
   ]
 })
