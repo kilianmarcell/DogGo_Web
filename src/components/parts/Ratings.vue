dc2b435548a25dd84ddf2ad7d5f3213e96b23d0d
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-30 23:08:34 +0200

    Értékelések megjelenítve helyenként

--- src/components/parts/Ratings.vue
+++ src/components/parts/Ratings.vue
@@ -1,8 +1,13 @@
 <template>
      <div class="text-wrapper p-5">
-          <div v-for="r in ratings" :key="r.id" class="text-inner w-75 fs-3">
-               <p>Értékelés: {{ r.stars }}</p>
-               <p>Leírás: {{ r.description }}</p>
+          <div v-for="r in ratings" :key="r.id" class="text-inner w-75 fs-4 mb-3">
+               <p>{{ r.description }}</p>
+               <div class="row float-start">
+                    <div class="col p-2 px-3" >
+                         <fa class="float-start mb-2" v-for="r in r.stars" :key="r.stars" :icon="['fas', 'star']"/>
+                         <p >Értékelés:  {{ r.stars }}</p>
+                    </div>
+               </div>
           </div>
      </div>
 </template>
@@ -35,7 +40,6 @@ export default {
 
      created() {
           this.locationRatingId = this.$route.params.ratingId
-          console.log(this.locationRatingId)
      }
 }
 </script>
