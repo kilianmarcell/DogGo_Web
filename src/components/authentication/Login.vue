4c765d24a43d7f42fc87fa8903bb7cc71956b411
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-31 22:01:02 +0200

    Login komponens javítva

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -8,24 +8,16 @@
                 <label class="fs-5 p-0">Felhasználónév</label>
                 <input type="text"
                     class="fs-5"
-                    v-model="this.state.registeredUser.username"
+                    v-model="this.registeredUser.username"
                     placeholder="Felhasználónév">
-
-                <span class="text-danger text-center" v-if="v$.registeredUser.username.$error">
-                    {{ v$.registeredUser.username.$errors[0].$message }}
-                </span>
             </div>
 
             <div class="row">
                 <label class="fs-5 p-0">Jelszó</label>
                 <input type="password"
                     class="fs-5"
-                    v-model="this.state.registeredUser.password"
+                    v-model="this.registeredUser.password"
                     placeholder="Jelszó">
-
-                <span class="text-danger text-center" v-if="v$.registeredUser.password.$error">
-                    {{ v$.registeredUser.password.$errors[0].$message }}
-                </span>
             </div>
         </div>
         <div class="pt-3">
@@ -44,9 +36,6 @@
 
 <script>
 import axios from "axios"
-import useVuelidate from "@vuelidate/core"
-import { required, minLength, maxLength, helpers } from "@vuelidate/validators"
-import { reactive, computed } from "vue"
 
 export default {
     name: 'Login',
@@ -58,69 +47,37 @@ export default {
                 email: "",
                 username: ""
             },
+            registeredUser: {
+                username: "",
+                password: ""
+            },
             token: "",
             error: false,
             errorMessage: ""
         }
     },
 
-    setup() {
-        const state = reactive({
-            registeredUser: {
-                username: "",
-                password: ""
-            }
-        })
-
-        const rules = computed(() => {
-            return {
-                registeredUser: {
-                    username: {
-                        required: helpers.withMessage('A felhasználónév mező kitöltése kötelező!', required),
-                        min: helpers.withMessage('A felhasználónévnek legalább 5 karakter hoszzúnak kell lennie!', minLength(5)),
-                        max: helpers.withMessage('A felhasználónév legfeljebb 20 karakter hoszzú lehet!', maxLength(20))
-                    },
-
-                    password: {
-                        required: helpers.withMessage('A jelszó mező kitöltése kötelező!', required),
-                        min: helpers.withMessage('A jelszónak legalább 8 karakter hoszzúnak kell lennie!', minLength(8))
-                    }
-                }
-            }
-        })
-
-        const v$ = useVuelidate(rules, state)
-
-        return {
-            state,
-            v$
-        }
-    },
-
     methods: {
         async login() {
-            this.v$.$validate()
-            if (!this.v$.$error) {
-                await axios
-                    .post('api/login', this.state.registeredUser)
-                    .then(response => (this.token = response.data.token))
-                    .then(this.error = false)
-                    .catch(error => {
-                        if (error.response.status == 401) {
-                            this.error = true
-                            this.errorMessage = "Helytelen felhasználónév vagy jelszó!"
-                        }
-                    })
+            await axios
+                .post('api/login', this.registeredUser)
+                .then(response => (this.token = response.data.token))
+                .then(this.error = false)
+                .catch(error => {
+                    if (error.response.status == 401) {
+                        this.error = true
+                        this.errorMessage = "Helytelen felhasználónév vagy jelszó!"
+                    }
+                })
 
-                if (!this.error) {
-                    await localStorage.setItem('token', this.token)
+            if (!this.error) {
+                await localStorage.setItem('token', this.token)
 
-                    let response = await axios
-                        .request({ url: 'api/user', method: 'get' })
-                            
-                    this.$store.dispatch('user', response.data)
-                    this.$router.push({ name: "Home" })
-                }
+                let response = await axios
+                    .request({ url: 'api/user', method: 'get' })
+                        
+                this.$store.dispatch('user', response.data)
+                this.$router.push({ name: "Home" })
             }
         }
     }
